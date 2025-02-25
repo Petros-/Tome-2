98984f0f1ce76f5544a97ab69449cdf4af112689
Peter Bentley <2217882+Petros-@users.noreply.github.com>
2025-02-24 21:07:38 -0800

    I have documents in my collection rendering on the page

--- tome-app/src/ArtworksList.jsx
+++ tome-app/src/ArtworksList.jsx
@@ -1,10 +1,31 @@
-import React from "react";
-import db from "./db.js"
+import React, {useEffect, useState} from "react";
+import db from "./db.js";
+import {collection, getDocs} from "firebase/firestore";
 
 function ArtworksList () {
+    const [artworks, setArtworks] = useState([]);
+    useEffect(() => {
+        const fetchArt = async () => {
+            try {
+                const querySnapshot = await getDocs(collection(db, "artworks"));
+                const items = querySnapshot.docs.map(doc => ({
+                    id: doc.id,
+                    ...doc.data()
+                }));
+                setArtworks(items);
+            } catch (error) {
+                console.log("An error happened:", error)
+            }
+        };
+        fetchArt();
+    }, [db]);
+
+    
     return (
         <>
-            Artworks list here
+            {artworks.map(artwork => (
+                <div key={artwork.id}>{artwork.title}, {artwork.createdAt?.toDate().toLocaleString()}</div>
+            ))}
         </>
     )
 }
