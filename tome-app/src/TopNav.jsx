065ab510b67a870920016eb5485adfad31bb763d
Peter Bentley <2217882+Petros-@users.noreply.github.com>
2025-02-25 22:36:41 -0800

    amazing how hard it was to get a simple svg to render for the logo

--- tome-app/src/TopNav.jsx
+++ tome-app/src/TopNav.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import {Link, Route} from "react-router-dom";
+import TomeSvg from './assets/Tome.svg';
 
 
 function TopNav () {
     return (
         <>
             <div>
-                <Link to="/" >Tome</Link>
+            <Link to="/"><img src={TomeSvg} alt="Tome Logo" width="100" height="16" /></Link>
                 <ul>
                     <li>
                         <Link to="/new" >Add</Link>
