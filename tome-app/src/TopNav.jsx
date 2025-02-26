de9f6104c9ad0c2b99c34b04e104a29248a6953a
Peter Bentley <2217882+Petros-@users.noreply.github.com>
2025-02-25 17:01:13 -0800

    I now have a way to get home

--- tome-app/src/TopNav.jsx
+++ tome-app/src/TopNav.jsx
@@ -1,10 +1,12 @@
 import React from "react";
+import {Link, Route} from "react-router-dom";
+import Home from "./Home";
 
 function TopNav () {
     return (
         <>
             <div>
-                <p>Tome</p>
+                <Link to="/" >Tome</Link>
                 <p>Sign in</p>
             </div>
         </>
