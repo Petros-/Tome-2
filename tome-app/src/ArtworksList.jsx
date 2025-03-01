b195ff691d5bd567750a174c77307b255e664ee7
Peter Bentley <2217882+Petros-@users.noreply.github.com>
2025-03-01 09:46:21 -0800

    I'm not happy with how this is going, so I'm going to try a different approach instead. Checking in this code in case I need to go back to it.

--- tome-app/src/ArtworksList.jsx
+++ tome-app/src/ArtworksList.jsx
@@ -1,51 +1,69 @@
 import React, { useEffect, useState } from "react";
-import db from "./db.js";
 import { collection, getDocs, deleteDoc, doc, query, orderBy, onSnapshot } from "firebase/firestore";
+import db from "./db.js";
 import { Link } from "react-router-dom";
 import Button from "./Button.jsx";
+
+import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth';
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
 
 
 function ArtworksList() {
     const [artworks, setArtworks] = useState([]);
     const [isLoading, setIsLoading] = useState(true);
     const [hasError, setHasError] = useState(false);
+    const [user, setUser] = useState({});
 
-
-    
+    // Configure Firebase.
+    const config = {
+        apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+        authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+        projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID
+    };
+    firebase.initializeApp(config);
 
     useEffect(() => {
+        const unregisteredAuthObserver = firebase.auth().onAuthStateChanged((user) => {
+            console.log(user.uid)
+            setUser(user)
+        })
+        return () => unregisteredAuthObserver
+    }, [user.uid])
+
+    console.log(user.uid);
+
+    useEffect(() => {
+
+        if (!user.uid) {
+            return
+        }
+
         const fetchArt = async () => {
             try {
-                // commenting this out so I can get the user's collection
-                // const querySnapshot = await getDocs(collection(db, "artworks"));
-                // const items = querySnapshot.docs.map(doc => ({
-                //     id: doc.id,
-                //     ...doc.data()
-                // }));
-                // setArtworks(items);
-
-                const userId = "xCUZ9GKaFmjApOlaTpH7";
-
-                const q = query(collection(db, "users", userId, "artworks"), orderBy("createdAt"));
+                
+                const q = query(collection(db, "users", user.uid, "artworks"), orderBy("createdAt"));
 
                 onSnapshot(q, (doc) => {
                     console.log("Current data:",);
                     setArtworks(doc.docs);
                 })
             } catch (error) {
-                setHasError(true)
                 console.log("An error happened:", error);
+                setHasError(true)
             } finally {
                 setIsLoading(false);
             }
         };
         fetchArt();
-    }, [db]);
+        return () => onSnapshot;
+
+    }, [user.uid]);
 
     const handleDelete = async (id) => {
         try {
             await deleteDoc(doc(db, 'artworks', id));
-            setArtworks(prevArtworks => prevArtworks.filter(artwork => artwork.id !== id));
+            // setArtworks(prevArtworks => prevArtworks.filter(artwork => artwork.id !== id));
 
         } catch (error) {
             console.error("Error with the delete:", error)
@@ -53,18 +71,25 @@ function ArtworksList() {
 
     }
 
+    if (isLoading) {
+        return <h2>Loading...</h2>
+    }
+
+    if (hasError) {
+        return <h2>There was an error. Peter wrote this</h2>
+    }
 
     return (
         <>
             <div className="absolute top-16 grid grid-rows-4 gap-4 h-full items-center">
-                {artworks.map(artwork => (
+                {artworks.map((artwork) => {
                     <div key={artwork.id} className="flex flex-row gap-3 items-center border border-gray-300 p-4"><Link to={`/artwork/${artwork.id}`} >{artwork.title}</Link>
                         {artwork.data().title} •
                         {artwork.data().createdAt?.toDate().toLocaleString()}
                         <Link to={`/edit/${artwork.id}`}><Button variant="secondary" size="small">Edit</Button></Link>
                         <Button onClick={() => handleDelete(artwork.id)} variant="secondary" size="small">Delete</Button>
                     </div>
-                ))}
+                })}
             </div>
         </>
     )
