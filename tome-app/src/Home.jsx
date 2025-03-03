63c79daf36d191784fae6d725d46dde653181fe3
Peter Bentley <2217882+Petros-@users.noreply.github.com>
2025-03-02 21:02:27 -0800

    trying to redeploy

--- tome-app/src/Home.jsx
+++ tome-app/src/Home.jsx
@@ -1,36 +1,37 @@
 import React from "react";
 import ArtworksList from "./ArtworksList";
 
-import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth';
-import firebase from 'firebase/compat/app';
-import 'firebase/compat/auth';
+// import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth';
+// import firebase from 'firebase/compat/app';
+// import 'firebase/compat/auth';
 
 function Home() {
 
-    // Configure Firebase.
-    const config = {
-        apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-        authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-        projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID
-    };
-    firebase.initializeApp(config);
+    // // Configure Firebase.
+    // const config = {
+    //     apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+    //     authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+    //     projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID
+    // };
+    // firebase.initializeApp(config); 
 
-    // Configure FirebaseUI.
-    const uiConfig = {
-        // Popup signin flow rather than redirect flow.
-        signInFlow: 'popup',
-        // Redirect to /signedIn after sign in is successful. Alternatively you can provide a callbacks.signInSuccess function.
-        signInSuccessUrl: '/',
-        // We will display Google and Facebook as auth providers.
-        signInOptions: [
-            firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-        ],
-    };
+    // // Configure FirebaseUI.
+    // const uiConfig = {
+    //     // Popup signin flow rather than redirect flow.
+    //     signInFlow: 'popup',
+    //     // Redirect to /signedIn after sign in is successful. Alternatively you can provide a callbacks.signInSuccess function.
+    //     signInSuccessUrl: '/',
+    //     // We will display Google and Facebook as auth providers.
+    //     signInOptions: [
+    //         firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+    //     ],
+    // };
 
     return (
         <>
-            <StyledFirebaseAuth uiConfig={uiConfig} firebaseAuth={firebase.auth()} />
+            {/* <StyledFirebaseAuth uiConfig={uiConfig} firebaseAuth={firebase.auth()} /> */}
             <ArtworksList />
+            Hellow
         </>
     )
 }
